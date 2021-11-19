69a9e44bd16f64cd56c92c5e964db66e1060dbfd
123 <123@123.com>
2021-11-19 18:02:56 +0800

    Update wallet injected

---
+++ src/hooks/index.ts
@@ -0,0 +1,87 @@
+import { Web3Provider } from '@ethersproject/providers'
+import { ChainId } from '@uniswap/sdk'
+import { useWeb3React as useWeb3ReactCore } from '@web3-react/core'
+import { Web3ReactContextInterface } from '@web3-react/core/dist/types'
+import { useEffect, useState } from 'react'
+import { isMobile } from 'react-device-detect'
+import { confluxconnect } from '../connectors'
+import { NetworkContextName } from '../constants'
+
+export function useActiveWeb3React(): Web3ReactContextInterface<Web3Provider> & { chainId?: ChainId } {
+  const context = useWeb3ReactCore<Web3Provider>()
+  const contextNetwork = useWeb3ReactCore<Web3Provider>(NetworkContextName)
+  return context.active ? context : contextNetwork
+}
+
+export function useEagerConnect() {
+  const { activate, active } = useWeb3ReactCore() // specifically using useWeb3ReactCore because of what this hook does
+  const [tried, setTried] = useState(false)
+
+  useEffect(() => {
+      confluxconnect.isAuthorized().then(isAuthorized => {
+      if (isAuthorized) {
+          activate(confluxconnect, undefined, true).catch(() => {
+          setTried(true)
+        })
+      } else {
+        if (isMobile && window.conflux) {
+            activate(confluxconnect, undefined, true).catch(() => {
+            setTried(true)
+          })
+        } else {
+          setTried(true)
+        }
+      }
+    })
+  }, [activate]) // intentionally only running on mount (make sure it's only mounted once :))
+
+  // if the connection worked, wait until we get confirmation of that to flip the flag
+  useEffect(() => {
+    if (active) {
+      setTried(true)
+    }
+  }, [active])
+
+  return tried
+}
+
+/**
+ * Use for network and injected - logs user in
+ * and out after checking what network theyre on
+ */
+export function useInactiveListener(suppress = false) {
+  const { active, error, activate } = useWeb3ReactCore() // specifically using useWeb3React because of what this hook does
+
+  useEffect(() => {
+    const { conflux } = window
+
+    if (conflux && conflux.on && !active && !error && !suppress) {
+      const handleChainChanged = () => {
+        // eat errors
+        activate(confluxconnect, undefined, true).catch(error => {
+          console.error('Failed to activate after chain changed', error)
+        })
+      }
+
+      const handleAccountsChanged = (accounts: string[]) => {
+        if (accounts.length > 0) {
+          // eat errors
+          activate(confluxconnect, undefined, true).catch(error => {
+            console.error('Failed to activate after accounts changed', error)
+          })
+        }
+      }
+
+      conflux.on('chainChanged', handleChainChanged)
+      conflux.on('accountsChanged', handleAccountsChanged)
+
+      return () => {
+        if (conflux.removeListener) {
+          conflux.removeListener('chainChanged', handleChainChanged)
+          conflux.removeListener('accountsChanged', handleAccountsChanged)
+        }
+      }
+    }
+    return undefined
+  }, [active, error, suppress, activate])
+}
